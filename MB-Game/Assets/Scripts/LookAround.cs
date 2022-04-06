1a8fd50c7db8204d07e77f554c274d2b5695b744
Felipe Andrey Miranda Nunes <senacfelipeandrey@gmail.com>
2022-04-06 18:29:47 -0300

    Vision Limit Updated
    Visão limitada a -40 / 5 agora. Jogador não olha totalmente para o teto e agora consegue olhar para o chão.

--- MB-Game/Assets/Scripts/LookAround.cs
+++ MB-Game/Assets/Scripts/LookAround.cs
@@ -34,7 +34,7 @@ public class LookAround : MonoBehaviour
         */
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 0f);
+        xRotation = Mathf.Clamp(xRotation, -40f, 5f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         
         playerBody.Rotate(Vector3.up * mouseX);
