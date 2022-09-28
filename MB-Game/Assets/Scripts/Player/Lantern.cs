68ce82280ecf25dee65753ed33c316b94e4e5654
Baltinha <63794832+Baltinha@users.noreply.github.com>
2022-09-28 16:01:26 -0300

    Sistema de Intensidade da Lanterna

--- MB-Game/Assets/Scripts/Player/Lantern.cs
+++ MB-Game/Assets/Scripts/Player/Lantern.cs
@@ -1,25 +1,49 @@
+using Unity.Mathematics;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Lantern : MonoBehaviour
 {
+    public GameManager manager;
     public Light luz;
     public float intencidade = 1;
     public bool luzAtiva;
+    public float distanceToInteract = 10;
+    private float valueRangeMax = 50;
+    private float valueIntencMax = 1000;
+    private float valueIntencMin = 50;
 
 
     private void Start()
     {
         GetComponentInChildren<Light>();
 
-        luz.intensity = intencidade;
+        luz.range = valueRangeMax;
+        luz.intensity = valueIntencMax;
     }
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hitInfo;
+
         if (Input.GetKeyDown(KeyCode.E))
         {
             luzAtiva = !luzAtiva;
             luz.gameObject.SetActive(luzAtiva);
+        }
+        if (luzAtiva)
+        {
+            if (Physics.Raycast(manager.cameraAtual.transform.position, manager.cameraAtual.transform.forward, out hitInfo, distanceToInteract, LayerMask.GetMask("Ground", "Scenary")))
+            {
+                //luz.intensity = math.lerp(valueIntencMin, valueIntencMax, 1 * Time.deltaTime);
+                luz.intensity = valueIntencMin;
+            }
+            else
+            {
+                //luz.intensity = math.lerp(valueIntencMax, valueIntencMin, 1 * Time.deltaTime);
+                luz.intensity = valueIntencMax;
+            }
+
         }
     }
 
